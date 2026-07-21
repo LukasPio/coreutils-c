92c8ac60166a6c8ddf7dddb13abd4bfd468a8f5f
LukasPio <lucaspio.galvao@gmail.com>
2026-07-21 20:02:48 -0300

    reduce dependencies

--- cat/main.c
+++ cat/main.c
@@ -1,49 +1,55 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
 
-#define EARG 1
-#define EPATH 2
-#define EOPEN 3
-#define EREAD 4
+#define EARG -1
 
-int getFd(char* path);
+int getFd(const char* path);
+int getStringLen(const char* s);
+void die(const char *exitmessage, const int errorcode);
+void handleErrno(const char *message);
 
 int main(int argc, char* argv[]) {
-
-    if (argc != 2) {
-        printf("Usage: cat [FILE]\n");
-        exit(EARG);
-    }
+    if (argc != 2) die("Usage: cat [FILE]\n", EARG);
+    
     char* filePath = argv[1];
     
     int fd = getFd(filePath);
     
     char buffer[4096];
-    int result;
+    ssize_t result;
 
-    while((result = read(fd, buffer, sizeof(buffer))) != 0) {
-        if (result == -1) {
-            perror("read");
-            exit(EREAD);
-        }
+    while((result = read(fd, buffer, sizeof(buffer))) > 0) {
         write(STDOUT_FILENO, buffer, result);
     }
+
+    if (result == -1) handleErrno(":22 error at read()");
 
     close(fd);
 
     return 0;
 }
 
-int getFd(char* path) {
+int getFd(const char* path) {
     int fd = open(path, O_RDONLY);
 
-    if (fd == -1) {
-        perror("open");
-        exit(EOPEN);
-    }
+    if (fd == -1) handleErrno(":33 error at open()");
 
     return fd;
+}
+
+int getStringLen(const char *s) {
+    int counter = 0;
+    while (s[counter] != '\0') counter++;
+    return counter;
+}
+
+void handleErrno(const char* message) {
+    die(message, errno);
+}
+
+void die(const char *exitmessage, const int errorcode) {
+    write(STDERR_FILENO, exitmessage, getStringLen(exitmessage));
+    write(STDERR_FILENO, "\n", 1);
+    _exit(errorcode);
 }
